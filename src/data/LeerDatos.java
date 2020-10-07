3ee3433e0f2483b4bc20383ef1633f058ab06aa8
working <working@DESKTOP-17KGHMK>
2020-10-07 15:13:41 -0500

    - se agregó la clase patrón - TODO, Agregar un tokenizador

---
+++ src/data/LeerDatos.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author working
+ */
+public class LeerDatos {
+    
+    public static ArrayList<Patron> tokenizarDataSet(){
+    // venatana para abrir el txt
+    
+    return null;
+    }
+    
+}
--- src/rp2021a/RP2021A.java
+++ src/rp2021a/RP2021A.java
@@ -5,6 +5,8 @@
  */
 package rp2021a;
 
+import data.Patron;
+
 /**
  *
  * @author working
@@ -16,6 +18,16 @@ public class RP2021A {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        /**distancias 
+        (2.4,5.6,3)   (5,6,3) d1:
+        (1,2) (6.7,7.8)  d2:
+        **/
+        Patron p1 = new Patron("", "", new double[]{1,2});
+        Patron p2 = new Patron("", "", new double[]{6.7,7.8});
+        System.out.println(p2.calcularDistancia(p1));
+        // TODO: TOKENIZADOR PARA PODER SEPARAR POR COMAS Y GENERAR UN COLECCION DE PATRONES
+        
     }
     
 }
